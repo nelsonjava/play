8b3fe62d3abaf3a77dd3098687dbec2d6cf40a2f
nelsonfer1 <nelsonjava@gmail.com>
2014-07-08 22:49:36 -0500

    first commit

--- simatool/app/controllers/Application.java
+++ simatool/app/controllers/Application.java
@@ -1,3 +1,6 @@
+// https://github.com/louth/play-mongo - A play framework module that provides integration with MongoDB
+// http://upcommons.upc.edu/pfc/bitstream/2099.1/13426/1/memoria.pdf - Tesis Play - Androy
+
 package controllers;
 
 import play.*;
