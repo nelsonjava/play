c0eb5df53261f4f18dacd4f4afd43269a4312c02
naif <nelsonjava@gmail.com>
2014-07-15 11:22:33 -0500

    first commit

--- simatool/app/models/com/naif/domains/models/SystemsModels.java
+++ simatool/app/models/com/naif/domains/models/SystemsModels.java
@@ -11,7 +11,17 @@ public class SystemsModels extends Model {
     public Float orden;
     public String name;
     public String observaciones;
-    
+
+/*
+    @OneToMany(mappedBy="post", cascade=CascadeType.ALL)
+    public List<Comment> comments;
+*/
+
+    @OneToMany(mappedBy="systemsModels",
+               fetch=FetchType.LAZY, targetEntity=void.class)
+    public Set<Domains> domains = new HashSet<Domains>(0);
+
+
     public String toString() {
         return name;
     }
